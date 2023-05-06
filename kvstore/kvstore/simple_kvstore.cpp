e6574d82642f1da828734ae43dc3df2e893027a7
Andrew Chen <andrew_z_chen@brown.edu>
2023-05-06 18:21:46 -0400

    Step 2.2

--- kvstore/kvstore/simple_kvstore.cpp
+++ kvstore/kvstore/simple_kvstore.cpp
@@ -24,10 +24,7 @@ bool SimpleKvStore::Put(const PutRequest* req, PutResponse*) {
 
 bool SimpleKvStore::Append(const AppendRequest* req, AppendResponse*) {
   // TODO (Part A, Steps 1 and 2): IMPLEMENT
-  // GetRequest* get_req = new GetRequest;
-  // get_req->key = req->key;
-  // GetResponse* get_res = new GetResponse;
-mtx.lock();
+  mtx.lock();
   for (std::map<std::string, std::string>::iterator it = kv_map.begin(); it != kv_map.end(); ++it) {
     // append to existing value if found
     if (it->first == req->key) {
@@ -43,32 +40,10 @@ mtx.lock();
   kv_map[req->key] = req->value;
   mtx.unlock();
   return true;
-
-
-  // // Append to existing value
-  // if (Get(get_req, get_res)) {
-  //   std::string combined_str;
-  //   combined_str.append(get_res->value);
-  //   combined_str.append(req->value);
-  //   kv_map[req->key] = combined_str;
-  //   delete get_req;
-  //   delete get_res;
-  //   return true;
-  // }
-  // // Create new value
-  // else {
-  //   kv_map[req->key] = req->value;
-  //   delete get_req;
-  //   delete get_res;
-  //   return true;
-  // }
 }
 
 bool SimpleKvStore::Delete(const DeleteRequest* req, DeleteResponse* res) {
   // TODO (Part A, Steps 1 and 2): IMPLEMENT
-  // GetRequest* get_req = new GetRequest;
-  // get_req->key = req->key;
-  // GetResponse* get_res = new GetResponse;
   // successfully gets the value
   mtx.lock();
   for (std::map<std::string, std::string>::iterator it = kv_map.begin(); it != kv_map.end(); ++it) {
@@ -83,22 +58,6 @@ bool SimpleKvStore::Delete(const DeleteRequest* req, DeleteResponse* res) {
   // key doesn't exist
   mtx.unlock();
   return false;
-
-
-
-
-  // if(Get(get_req, get_res)) {
-  //   std::string result = get_res->value;
-  //   res->value = result;
-  //   kv_map.erase(req->key);
-  //   delete get_req;
-  //   delete get_res;
-  //   return true;
-  // }
-  // // key doesn't exist
-  // delete get_req;
-  // delete get_res;
-  // return false;
 }
 
 bool SimpleKvStore::MultiGet(const MultiGetRequest* req,
@@ -123,30 +82,6 @@ bool SimpleKvStore::MultiGet(const MultiGetRequest* req,
   }
   mtx.unlock();
   return true;
-  
-  
-  
-  
-  
-  // mtx.lock();
-  // GetRequest* get_req = new GetRequest;
-  // GetResponse* get_res = new GetResponse;
-  // for (std::string key : req->keys) {
-  //   get_req->key = key;
-  //   // Key not found
-  //   if(!Get(get_req, get_res)) {
-  //     delete get_req;
-  //     delete get_res;
-  //     mtx.unlock();
-  //     return false;
-  //   }
-  //   // Add value 
-  //   (res->values).push_back(get_res->value);
-  // }
-  // delete get_req;
-  // delete get_res;
-  // mtx.unlock();
-  // return true;
 }
 
 bool SimpleKvStore::MultiPut(const MultiPutRequest* req, MultiPutResponse*) {
@@ -161,34 +96,6 @@ bool SimpleKvStore::MultiPut(const MultiPutRequest* req, MultiPutResponse*) {
   }
   mtx.unlock();
   return true;
-  
-  
-  
-  
-  
-  
-  
-  // mtx.lock();
-  // if (req->keys.size() != req->values.size()) {
-  //   mtx.unlock();
-  //   return false;
-  // }
-  // PutRequest* put_req = new PutRequest;
-  // PutResponse* put_res = new PutResponse;
-  // for (int i = 0; i < (int)req->keys.size(); i++) {
-  //   put_req->key = (req->keys)[i];
-  //   put_req->value = (req->values)[i];
-  //   if (!Put(put_req, put_res)) {
-  //     delete put_req;
-  //     delete put_res;
-  //     mtx.unlock();
-  //     return false;
-  //   }
-  // }
-  // delete put_req;
-  // delete put_res;
-  // mtx.unlock();
-  // return true;
 }
 
 std::vector<std::string> SimpleKvStore::AllKeys() {
