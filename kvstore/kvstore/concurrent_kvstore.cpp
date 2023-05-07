2ca3311988afb3874a497d69b6fe452f3547eacb
Andrew Chen <andrew_z_chen@brown.edu>
2023-05-07 05:38:07 -0400

    Step 5 with 1 shared_mutex

--- kvstore/kvstore/concurrent_kvstore.cpp
+++ kvstore/kvstore/concurrent_kvstore.cpp
@@ -4,86 +4,101 @@
 
 bool ConcurrentKvStore::Get(const GetRequest* req, GetResponse* res) {
   // TODO (Part A, Steps 4 and 5): IMPLEMENT
+  this->store.shared_mtx.lock_shared();
+  // std::shared_lock guard(this->store.shared_mtx);
   std::optional<DbItem> val = this->store.getIfExists(this->store.bucket(req->key), req->key);
   if (val.has_value()) {
     res->value = val.value().value;
+    this->store.shared_mtx.unlock_shared();
     return true;
   }
+  this->store.shared_mtx.unlock_shared();
   return false;
 }
 
 bool ConcurrentKvStore::Put(const PutRequest* req, PutResponse*) {
   // TODO (Part A, Steps 4 and 5): IMPLEMENT
+  this->store.shared_mtx.lock();
   this->store.insertItem(this->store.bucket(req->key), req->key, req->value);
+  this->store.shared_mtx.unlock();
   return true;
 }
 
 bool ConcurrentKvStore::Append(const AppendRequest* req, AppendResponse*) {
   // TODO (Part A, Steps 4 and 5): IMPLEMENT
+  this->store.shared_mtx.lock();
   size_t bucket_num = this->store.bucket(req->key);
   std::optional<DbItem> val = this->store.getIfExists(bucket_num, req->key);
   
   if(val.has_value()) {
     // Append existing value
     this->store.insertItem(bucket_num, req->key, (val.value().value).append(req->value));
+    this->store.shared_mtx.unlock();
     return true;
   }
   // Add new value if key not found
   this->store.insertItem(bucket_num, req->key, req->value);
+  this->store.shared_mtx.unlock();
   return true;
 }
 
 bool ConcurrentKvStore::Delete(const DeleteRequest* req, DeleteResponse* res) {
   // TODO (Part A, Steps 4 and 5): IMPLEMENT
+  this->store.shared_mtx.lock();
   size_t bucket_num = this->store.bucket(req->key);
   std::optional<DbItem> val = this->store.getIfExists(bucket_num, req->key);
   if (val.has_value()) {
     res->value = val.value().value;
   }
-  return this->store.removeItem(bucket_num, req->key);
-  
-  // res->value = getIfExists(bucket_num, req->key).value;
-  // if(!(res->value)) {
-  //   return false
-  // }
-  // remove
-  // return true;
+  bool ret = this->store.removeItem(bucket_num, req->key);
+  this->store.shared_mtx.unlock();
+  return ret;
 }
 
 bool ConcurrentKvStore::MultiGet(const MultiGetRequest* req,
                                  MultiGetResponse* res) {
   // TODO (Part A, Steps 4 and 5): IMPLEMENT
+  this->store.shared_mtx.lock_shared();
+  // std::shared_lock guard(this->store.shared_mtx);
   res->values = {};
   for (std::string key : req->keys) {
     size_t bucket_num = this->store.bucket(key);
     std::optional<DbItem> val = this->store.getIfExists(bucket_num, key);
     if (!val.has_value()) {
+      this->store.shared_mtx.unlock_shared();
       return false;
     }
     (res->values).push_back(val.value().value);
   }
+  this->store.shared_mtx.unlock_shared();
   return true;
 }
 
 bool ConcurrentKvStore::MultiPut(const MultiPutRequest* req,
                                  MultiPutResponse*) {
   // TODO (Part A, Steps 4 and 5): IMPLEMENT
+  this->store.shared_mtx.lock();
   if (req->keys.size() != req->values.size()) {
+    this->store.shared_mtx.unlock();
     return false;
   }
   for (int i = 0; i < (int) req->keys.size(); i++) {
     this->store.insertItem(this->store.bucket((req->keys)[i]), (req->keys)[i], (req->values)[i]);
   }
+  this->store.shared_mtx.unlock();
   return true;
 }
 
 std::vector<std::string> ConcurrentKvStore::AllKeys() {
   // TODO (Part A, Steps 4 and 5): IMPLEMENT
+  // std::shared_lock guard(this->store.shared_mtx);
+  this->store.shared_mtx.lock_shared();
   std::vector<std::string> keys = {};
   for (std::list<DbItem> bucket : this->store.buckets) {
     for (DbItem item : bucket) {
       keys.push_back(item.key);
     }
   }
+  this->store.shared_mtx.unlock_shared();
   return keys;
 }
--- kvstore/kvstore/concurrent_kvstore.hpp
+++ kvstore/kvstore/concurrent_kvstore.hpp
@@ -45,6 +45,7 @@ class DbMap {
 
   // TODO (Part A, Step 5): You will need to add fields to synchronize access to
   // the hashmap buckets!
+  std::shared_mutex shared_mtx;
 
   // Return the index of the bucket to search for `key`.
   size_t bucket(std::string key) const {
