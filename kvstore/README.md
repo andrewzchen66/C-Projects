edc698cc7b4255c7bdb199e454ad12c4d083782d
Andrew Chen <andrew_z_chen@brown.edu>
2023-05-10 01:27:51 -0400

    Final Submission

--- kvstore/README.md
+++ kvstore/README.md
@@ -4,11 +4,11 @@ Project 5A - Concurrent Store
 <!-- TODO: Fill this out. -->
 
 ## Design Overview:
-
+None
 ## Collaborators:
-
+None
 ## Extra Credit Attempted:
-
+None
 ## How long did it take to complete the Concurrent Store portion of KVStore?
 25 hours
 <!-- Enter an approximate number of hours that you spent actively working on the project. -->
@@ -20,11 +20,36 @@ Project 5B - Distributed Store
 <!-- TODO: Fill this out. -->
 
 ## Design Overview:
+1. Who was your stakeholder pair? (<1 sentence)
+
+Pair #5: Matt Bleat vs. The Center for Changing Our Campus Culture
+
+2. What kind of delete did you implement and why? Explain your decisions as if you were reporting to an ethical auditor who will determine whether your design is justified. Highlight and explain what you think is the most compelling reason that supports the specific kind of deletion you’ve implemented (1-2 short paragraphs)
+
+My deletion implemention will remove all of the user's personal posts, and remove the affiliation between the user's personal posts and any replies to those posts. The deletion will also remove all tags of @user_id in other users' posts and comments, as well as any mentions of the user's username. This includes hashtags that includes the username, as they can be used to directly find the user's account in question (we are not actually deleting the user's account, so it is still searchable by anyone on the platform). However, the posts and replies made by other users will remain, as deleting these posts would infringe on the other users' freedom of speech/expression.
+
+This implementation aligns with Article 17 GDPR's Right to Erasure, because it gives data subjects "the right to obtain from the controller the erasure of personal data concerning him or her," where we define "personal data" as any text containing the user's username or a tag of the user. This effectively removes any surface-level connection on Tweeter between the the controversial topics and the user, Matt. However, the controversial posts and replies related to the user will remain, along with any other hashtags or tags. This serves as a balance with the opposing stakeholder, The Center for Changing Our Campus Culture. Posts will still contain tags to @brownusports, so the activism and attempts to draw attention to Brown's institution will still be there on the platform. Additionally, it would not be hard for the University and those involved in the situation to make the connection between Matt and what the posts/replies are referring to, so it is perfectly possible for his offer of admission to be rescinded still. So, the deletion essentially anonymizes Matt's identity on Tweeter and prevents others from discussing him in a targeted, hurtful way while still allowing the situation to be talked about extensively and for the Center for Changing Our Campus Culture's missions of public good and Title IX awareness to be fulfilled.
+
+3. What are the shortcomings of your implementation? Who are some short term and/or long term stakeholders (beyond the ones we’ve asked you to consider) who could be adversely affected by your decision? (1-2 short paragraphs)
+
+One shortcoming of this implementation is since hashtags aren't tied down to one user or idea, it is possible that by censoring a hashtag that contains Matt's name, we are inadvertently censoring a completely different use for the hashtag that is unrelated to this situation. Additional natural language processing would need to be implemented to determine the intent of the post that's using the hashtag, so that only those related to the specified user would be deleted. 
+
+Additionally, the people who are tangentially involved in the situation, such as Matt's family, classmates, teammates, the victims in the sexual harassment allegations, or Brown University could face the brunt of the backlash online in place of Matt. And now that Matt's connection is removed from the narrative, the negativity previously associated with Matt's actions could now be fully associated with Brown, which could lead to misconceptions and misleading narrative online that could tarnish the reputation of the University.
+
+
+4. How might your approach to this assignment change if you were asked to consider the interests of other stakeholders not mentioned in the scenario? (1-2 short paragraphs)
+
+I would first consider the stakeholder position of Brown University, who have already ruled on the situation and concluded that there was insufficient evidence to support those allegations and declined to take further action. The position of the University in this situation would likely be to reduce Matt's and their association with the situation as much as possible. However, since it is only Matt that is invoking the GDPR Right to Erasure, there is minimal additional actions that can be taken aside from completely removing the posts, which would go against the other user's freedom of speech/expression. 
+
+Another main stakeholder with be the victims in the sexual harassment allegations, who depending on their position might take different sides in the conflict. They could decide to want nothing to do with this, which would require our deletion algorithm to take into account the privacy of additional people, which is not feasible due to same reason as above. They could also take the side of the Center for Changing Our Campus Culture and want the situation to be greater publicized in the hopes of taking further legal action in the sexual harassment case. In this case, the erasure would no longer be able to go through as it is being used "for the establishment, exercise or defence of legal claims." In this case, we would have to honor any legally binding contract or warrant issued by the court and maintain any court-admissable evidence on Tweeter. This would mean keeping all of the posts related to Matt's situation exactly as they were originally, and not censoring hashtags or comments.
 
 ## Collaborators:
+None
 
 ## Extra Credit Attempted:
+None
 
 ## How long did it take to complete the Distributed Store portion of KVStore?
 
 <!-- Enter an approximate number of hours that you spent actively working on the project. -->
+20
--- kvstore/client/simple_client.cpp
+++ kvstore/client/simple_client.cpp
@@ -1,5 +1,10 @@
 #include "simple_client.hpp"
 
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+
 std::optional<std::string> SimpleClient::Get(const std::string& key) {
   std::shared_ptr<ServerConn> conn = connect_to_server(this->server_addr);
   if (!conn) {
@@ -144,6 +149,142 @@ bool SimpleClient::GDPRDelete(const std::string& user) {
   //
   // Assume the `user` arugment is a user ID such as "user_1".
 
-  cerr_color(RED, "GDPR deletion is unimplemented!");
-  return false;
+  // Part 1: Delete all of user's personal posts, and remove user's affiliation with reply posts
+  std::string user_posts_key(user + "_posts");
+  std::optional<std::string> user_posts_opt = Get(user_posts_key); //Obtain a string of all the user's personal posts
+  
+  if (user_posts_opt.has_value()) { //if user has any posts
+    std::string user_posts = user_posts_opt.value();
+    std::istringstream iss_user_posts(user_posts);
+    std::string user_post;
+    // std::string user_post = strtok(user_posts, ",");
+    while (std::getline(iss_user_posts, user_post, ',')) { //For each of the user's personal posts:
+      // Get rid of trailing whitespace if it exists
+      if (user_post[user_post.length() - 1] == ' ') {
+        user_post.erase(user_post.length() - 1, 1);
+      }
+
+      //Delete the affiliation between the user's personal posts and their replies, so the replies would essentially be treated as just any other unaffiliated post.  
+      // Implement this by deleting the post_id_replies key
+      std::string post_replies_key = user_post + "_replies";
+      Delete(post_replies_key);
+
+      Delete(user_post); //Delete the actual post
+      // user_post = strtok(NULL, ",") // Get next post to delete
+    }
+
+    // Update the value of user_id_posts's key-value pair as empty to represent the newly deleted posts of the user. 
+    // We aren't deleting the user_id_posts category entirely because the user's account isn't deleted, just all of their posts. User's can choose to delete their account if they wish but that isn't part of GDPRDelete's functionality. 
+    Put(user_posts_key, "");
+  }
+  
+  // ----------------------------------------------------------------------------
+  // Part 2: Delete all @user tags, username occurences, and hashtags containing the user from other people's posts and replies
+  std::string user_tag = "@" + user; //string representing the user tag
+  std::string user_name = Get(user).value(); //string representing the username
+  user_name.erase(user_name.length() - 1, 1);
+
+  // Convert username to lowercase
+  for (char& c : user_name) {
+    c = std::tolower(c);
+  }
+
+  // Parse the user_name by " " into a vector of individual words
+  std::istringstream iss_user_name(user_name);
+  std::string user_name_segment;
+  std::vector<std::string> parsed_user_name;
+  while (std::getline(iss_user_name, user_name_segment, ' ')) {
+    // Get rid of trailing whitespace if it exists
+    if (user_name_segment[user_name_segment.length() - 1] == ' ') {
+      user_name_segment.erase(user_name_segment.length() - 1, 1);
+    }
+    parsed_user_name.push_back(user_name_segment);
+  }
+
+  //Iterate through every user on the platform
+  std::istringstream iss_all_users(Get("all_users").value()); //Get string of all user ids on the platform
+  std::string some_user_id;
+  while (std::getline(iss_all_users, some_user_id, ',')) { 
+    // Get rid of trailing whitespace if it exists
+    if (some_user_id[some_user_id.length() - 1] == ' ') {
+      some_user_id.erase(some_user_id.length() - 1, 1);
+    }
+    //If the current user has any posts
+    if (Get(some_user_id + "_posts").has_value()) { 
+      std::istringstream iss_posts_per_user(Get(some_user_id + "_posts").value());
+      std::string some_post_id;
+      //Iterate through every one of the user's posts
+      while(std::getline(iss_posts_per_user, some_post_id, ',')) { 
+        // Get rid of trailing whitespace if it exists
+        if (some_post_id[some_post_id.length() - 1] == ' ') {
+          some_post_id.erase(some_post_id.length() - 1, 1);
+        }
+        std::string post_content = Get(some_post_id).value(); //Get the post content from kvstore
+        
+        // replace all instances of "@user_id" tags with ""
+        std::string::size_type pos = post_content.find(user_tag);
+        while (pos != std::string::npos) {
+          post_content.replace(pos, user_tag.length(), "");
+          pos = post_content.find(user_tag);
+        }
+
+        // Convert post content to lowercase
+        std::string lower_post_content = post_content;
+        for (char& c : lower_post_content) {
+          c = std::tolower(c);
+        }
+
+        // Delete all hashtags containing the username from the post
+        // First Parse the post_content into vectors of separate words
+        std::istringstream iss_post(post_content);
+        std::string post_word;
+        std::vector<std::string> parsed_post_content;
+        while (std::getline(iss_post, post_word, ' ')) {
+          parsed_post_content.push_back(post_word);
+        }
+        // Next parse lower_post_content into vectors of separate words
+        std::istringstream iss_lower_post(lower_post_content);
+        std::string lower_post_word;
+        std::vector<std::string> lower_parsed_post_content;
+        while (std::getline(iss_lower_post, lower_post_word, ' ')) {
+          lower_parsed_post_content.push_back(lower_post_word);
+        }
+        post_content = "";
+        // Loop through every word in the post
+        for (int i = 0; i < (int)parsed_post_content.size(); i++) {
+          // Determine if the word is hashtag
+          if (lower_parsed_post_content[i][0] == '#') {
+            // Loop through every word in the username to check if the part is located in the hashtag
+            for (std::string name : parsed_user_name) {
+              pos = (lower_parsed_post_content[i]).find(name);
+              // Remove the hashtag if contains any part of the name
+              if (pos != std::string::npos) {
+                lower_parsed_post_content[i] = "";
+                parsed_post_content[i] = "";
+                // lower_parsed_post_content.erase(lower_parsed_post_content.begin() + i);
+                // parsed_post_content.erase(parsed_post_content.begin() + i);
+              }
+            }
+          }
+          // Recombine the post vector into one string 
+          post_content.append(parsed_post_content[i]);
+          post_content += " ";
+        }
+
+        // Replace all mentions of the username from the post with "TweetTweet"
+        // We only delete mentions of the entire username, e.g. for "matt bleat", instances of "matt" or "bleat" would not be censored.
+        // Potential drawback: we don't censor every instance of the username.
+        pos = lower_post_content.find(user_name);
+        while (pos != std::string::npos) {
+          post_content.replace(pos, user_name.length(), "TweetTweet"); //Censor the actual post content 
+          lower_post_content.replace(pos, user_name.length(), "TweetTweet"); //Censor the lowercase version of the post content 
+          pos = lower_post_content.find(user_name);
+        }
+
+        // Put the edited post contents back into the kv store
+        Put(some_post_id, post_content);
+      }
+    }
+  }
+  return true;
 }
